c061d371141a1163557a0cd83656a2fb2ec3b419
rios-95 <rios.ba95@gmail.com>
2020-06-08 01:48:41 -0300

    estructuras broker

--- Broker/broker.c
+++ Broker/broker.c
@@ -46,14 +46,22 @@ bool existeArchivoConfig(char* path){
 	}
 }
 
+void encolarNewPokemon(t_new_pokemon* mensaje){
+	t_nodo_cola_new* nodo = malloc(sizeof(t_nodo_cola_new));
+	nodo->mensaje = mensaje;
+	nodo->susc_enviados = list_create();
+	nodo->susc_ack = list_create();
+
+	list_add(cola_new->nodos,nodo);
+}
 void atenderCliente(int socket_cliente){
 	printf("Atender Cliente %d: \n", socket_cliente);
 	op_code cod_op = recibirOperacion(socket_cliente);
 	switch(cod_op){
 		case NEW_POKEMON:{
-
 			t_new_pokemon* new_pokemon = recibirNewPokemon(socket_cliente);
 			log_info(logBroker, "Llego el mensaje NEW_POKEMON.");
+			encolarNewPokemon(new_pokemon);
 			break;
 		}
 		case APPEARED_POKEMON:{
@@ -62,25 +70,21 @@ void atenderCliente(int socket_cliente){
 			break;
 		}
 		case CATCH_POKEMON:{
-
 			t_catch_pokemon* catch_pokemon = recibirCatchPokemon(socket_cliente);
 			log_info(logBroker, "Llego el mensaje CATCH_POKEMON.");
 			break;
 		}
 		case CAUGHT_POKEMON:{
-
 			t_caught_pokemon* caught_pokemon= recibirCaughtPokemon(socket_cliente);
 			log_info(logBroker, "Llego el mensaje CAUGHT_POKEMON.");
 			break;
 		}
 		case GET_POKEMON:{
-
 			t_get_pokemon* get_pokemon = recibirGetPokemon(socket_cliente);
 			log_info(logBroker, "Llego el mensaje GET_POKEMON.");
 			break;
 		}
 		case LOCALIZED_POKEMON:{
-
 			t_localized_pokemon* localized_pokemon = recibirLocalizedPokemon(socket_cliente);
 			log_info(logBroker, "Llego el mensaje LOCALIZED_POKEMON.");
 			break;
@@ -109,35 +113,45 @@ void atenderCliente(int socket_cliente){
 	}
 }
 
-void inicializarColas(){
+void inicializarEstructuras(){
 	crearConfigBroker();
 
-	colaNewPokemon = list_create();
-	colaAppearedPokemon = list_create();
-	colaCatchPokemon = list_create();
-	colaCaughtPokemon = list_create();
-	colaGetPokemon = list_create();
-	colaLocalizedPokemon = list_create();
+	cola_new->nodos = list_create();
+	cola_new->suscriptores = list_create();
+
+	cola_appeared->nodos = list_create();
+	cola_appeared->suscriptores = list_create();
+
+	cola_get->nodos = list_create();
+	cola_get->suscriptores = list_create();
+
+	cola_localized->nodos = list_create();
+	cola_localized->suscriptores = list_create();
+
+	cola_catch->nodos = list_create();
+	cola_catch->suscriptores = list_create();
+
+	cola_caught->nodos = list_create();
+	cola_caught->suscriptores = list_create();
 }
 
 int main(void){
 	logBroker = log_create("broker.log", "LOG", 0, LOG_LEVEL_INFO);
 	logBrokerInterno= log_create("brokerInterno.log", "LOG", 0, LOG_LEVEL_INFO);
-	inicializarColas();
+	inicializarEstructuras();
 
 	int socketServidorBroker = crearSocketServidor(config_broker->ip_broker, config_broker->puerto_broker);
 
 	if(socketServidorBroker == -1){
 		printf("No se pudo crear el Servidor Broker.");
+		return -1;
 	}else{
 		printf("Socket Servidor %d.\n", socketServidorBroker);
 	}
 
 	while(1){
-		//Aviso, si falla (como cuando se bloquea el puerto) hace un loop infinito
 		int cliente = aceptarCliente(socketServidorBroker);
 		atenderCliente(cliente);
 	}
-
 	if(socketServidorBroker != -1) close(socketServidorBroker);
 }
--- Broker/broker.h
+++ Broker/broker.h
@@ -28,17 +28,7 @@ typedef struct{
 	int frecuencia_compatacion;
 } t_configuracion;
 
-typedef struct{
-	t_list* suscriptores_yaEnviados;
-	t_list* suscriptores_retornaronACK;
-} t_mensaje_suscriptor;
-
-typedef struct{
-	t_list* suscriptores;
-} t_nodo_cola;
-
 typedef struct {
-	uint32_t largo_nombre;
 	char* nombre_pokemon;
 	uint32_t pos_x;
 	uint32_t pos_y;
@@ -46,7 +36,6 @@ typedef struct {
 } t_new_pokemon_memoria;
 
 typedef struct {
-	uint32_t largo_nombre;
 	char* nombre_pokemon;
 	uint32_t pos_x;
 	uint32_t pos_y;
@@ -75,15 +64,55 @@ t_config* config_ruta;
 t_log* logBrokerInterno;
 t_log* logBroker;
 
-t_list* colaNewPokemon;
-t_list* colaAppearedPokemon;
-t_list* colaCatchPokemon;
-t_list* colaCaughtPokemon;
-t_list* colaGetPokemon;
-t_list* colaLocalizedPokemon;
+typedef struct{
+	t_list* nodos; //lista con t_nodos_x
+	t_list* suscriptores;
+}t_cola;
+
+//Nodos de cada cola
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_new_pokemon* mensaje;
+} t_nodo_cola_new;
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_appeared_pokemon* mensaje;
+} t_nodo_cola_appeared;
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_get_pokemon* mensaje;
+} t_nodo_cola_get;
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_localized_pokemon* mensaje;
+} t_nodo_cola_localized;
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_catch_pokemon* mensaje;
+} t_nodo_cola_catch;
+typedef struct{
+	t_list* susc_enviados;
+	t_list* susc_ack;
+	t_caught_pokemon* mensaje;
+} t_nodo_cola_caught;
+
 
 int crearConfigBroker();
 bool existeArchivoConfig(char* path);
 void atenderCliente(int socket_cliente);
+void encolarNewPokemon(t_new_pokemon* new);
+
+t_cola* cola_new;
+t_cola* cola_appeared;
+t_cola* cola_get;
+t_cola* cola_localized;
+t_cola* cola_catch;
+t_cola* cola_caught;
+
 
 #endif /* BROKER_H_ */
