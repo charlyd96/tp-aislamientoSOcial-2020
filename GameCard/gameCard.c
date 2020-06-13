a3b329258264777030ea85acc330ee7a19c993d1
Maldonado-J <maldonado.johana3@gmail.com>
2020-06-13 15:59:02 -0300

    Creacion de directorios

--- GameCard/gameCard.c
+++ GameCard/gameCard.c
@@ -7,13 +7,39 @@
 
 #include "gamecard.h"
 
-int crearConfigGamecard(){
+int main(void){
 
-	t_log* logGamecard = log_create("gamecard.txt", "LOG", 0, LOG_LEVEL_INFO);
+	logGamecard = log_create("gamecard.log", "Gamecard", 0, LOG_LEVEL_INFO);
+	logInterno = log_create("gamecardInterno.log", "GamecardInterno", 0, LOG_LEVEL_INFO);
+	crear_config_gamecard();
 
-    log_info(logGamecard, "Se hizo el Log.\n");
+	int socketServidorGamecard = crearSocketServidor(config_gamecard->ip_gamecard, config_gamecard->puerto_gamecard);
 
-	if (!existeArchivoConfig(pathGamecardConfig)) {
+	if(socketServidorGamecard == -1){
+		log_info(logGamecard, "No se pudo crear el Servidor GameCard");
+	}else{
+		log_info(logGamecard, "Socket Servidor %d\n", socketServidorGamecard);
+		while(1){
+					log_info(logInterno,"While de aceptar cliente");
+					int cliente = aceptarCliente(socketServidorGamecard);
+
+					pthread_t hiloCliente;
+					pthread_create(&hiloCliente, NULL, (void*)atender_cliente, &cliente);
+
+					pthread_detach(hiloCliente);
+				}
+		close(socketServidorGamecard);
+		log_info(logGamecard, "Se cerro el Soket Servidor %d\n", socketServidorGamecard);
+	}
+
+}
+
+
+int crear_config_gamecard(){
+
+    log_info(logGamecard, "Inicializacion del log.\n");
+
+	if (!existe_archivo(pathGamecardConfig)) {
 		log_error(logGamecard, "Verificar path del archivo \n");
 	    return -1;
 	}
@@ -36,7 +62,7 @@ int crearConfigGamecard(){
 	return 0;
 }
 
-bool existeArchivoConfig(char* path){
+bool existe_archivo(char* path){
 	FILE * file = fopen(path, "r");
 	if(file!=NULL){
 		fclose(file);
@@ -46,33 +72,19 @@ bool existeArchivoConfig(char* path){
 	}
 }
 
-void atenderCliente(int socket_cliente){
-	printf("Atender cliente %d \n", socket_cliente);
+void atender_cliente(int* socket){
+	int socket_cliente = *socket;
+	log_info(logGamecard, "Atender cliente %d \n",socket_cliente);
 	op_code cod_op = recibirOperacion(socket_cliente);
 	switch(cod_op){
 		case NEW_POKEMON:
-			printf("Recibi NEW_POKEMON");
-			t_new_pokemon* new_pokemon= recibirNewPokemon(socket_cliente);
-			printf("nombre: %s\n x: %d\n y: %d\n cant: %d\n id: %d\n",new_pokemon->nombre_pokemon,new_pokemon->pos_x,new_pokemon->pos_y,new_pokemon->cantidad,new_pokemon->id_mensaje);
-			/**
-			 * tratarRecepcionNewPokemon(new_pokemon)
-			 */
+			atender_newPokemon(socket_cliente);
 			break;
 		case GET_POKEMON:
-			printf("Recibi GET_POKEMON\n");
-			t_get_pokemon* get_pokemon= recibirGetPokemon(socket_cliente);
-			printf(" nombre: %s\n id: %d\n",get_pokemon->nombre_pokemon,get_pokemon->id_mensaje);
-			/**
-			 * tratarRecepcionGetPokemon(get_pokemon)
-			 */
+			atender_getPokemon(socket_cliente);
 			break;
 		case CATCH_POKEMON:
-			printf("Recibi CATCH_POKEMON\n");
-			t_catch_pokemon* catch_pokemon= recibirCatchPokemon(socket_cliente);
-			printf(" nombre: %s\n x: %d\n y: %d\n id: %d\n",catch_pokemon->nombre_pokemon,catch_pokemon->pos_x,catch_pokemon->pos_y,catch_pokemon->id_mensaje);
-			/**
-			 * tratarRecepcionCatchPokemon(catch_pokemon)
-			 */
+			atender_catchPokemon(socket_cliente);
 			break;
 		default:
 			printf("La operación no es correcta");
@@ -80,22 +92,51 @@ void atenderCliente(int socket_cliente){
 	}
 }
 
-int main(void){
+void atender_newPokemon(int socket){
+	t_new_pokemon* new_pokemon = recibirNewPokemon(socket);
+	log_info(logGamecard, "Recibi NEW_POKEMON %s %d %d %d [%d]\n",new_pokemon->nombre_pokemon,new_pokemon->pos_x,new_pokemon->pos_y,new_pokemon->cantidad,new_pokemon->id_mensaje);
 
-	crearConfigGamecard();
+	char* pathMetadata = string_from_format("%s/Files/%s/Metadata.bin", config_gamecard->punto_montaje_tall_grass, new_pokemon->nombre_pokemon);
 
-	int socketServidorGamecard = crearSocketServidor(config_gamecard->ip_gamecard, config_gamecard->puerto_gamecard);
-
-	if(socketServidorGamecard == -1){
-		printf("No se pudo crear el Servidor GameCard");
-	}else{
-		printf("Socket Servidor %d\n", socketServidorGamecard);
-
-		int cliente = aceptarCliente(socketServidorGamecard);
-
-		atenderCliente(cliente);
+	if(existe_archivo(pathMetadata)){
+		data_pokemon = malloc(sizeof(t_metadata));
+		metadata = config_create(pathMetadata);
+		if(metadata != NULL){
+			data_pokemon->directory=config_get_string_value(metadata, "DIRECTORY");
+			data_pokemon->size=config_get_int_value(metadata,"SIZE");
+			data_pokemon->blocks=config_get_array_value(metadata, "BLOCKS");
+			data_pokemon->open=config_get_string_value(metadata,"OPEN");
+		}
+		else {
+			log_error(logInterno, "ERROR: No se pudo levantar la Metadata de %s.\n", new_pokemon->nombre_pokemon);
+		}
 	}
+	else{
+		crear_directorio_pokemon(new_pokemon->nombre_pokemon);
+	}
+}
 
-	if(socketServidorGamecard != -1) close(socketServidorGamecard);
+void crear_directorio_pokemon(char* pokemon){
+	char* pathFiles = string_from_format("%s/Files", config_gamecard->punto_montaje_tall_grass);
+	int cambioAPathFiles = chdir(pathFiles);
+	if(cambioAPathFiles){
+		string_capitalized(pokemon);
+		int creoDirectorio = mkdir(pokemon,0700);
+		if(creoDirectorio){
+			log_info(logInterno, "Se creo el directorio /%s", pokemon);
+			//CREAR METADATA
+		}
+		else log_error(logInterno, "ERROR: No se pudo crear directorio /%s", pokemon);
+	}
+	else log_error(logInterno, "ERROR: No se cambio de directorio\n");
+}
 
+void atender_getPokemon(int socket){
+	t_get_pokemon* get_pokemon= recibirGetPokemon(socket);
+	log_info(logGamecard, "Recibi GET_POKEMON %s [%d]\n",get_pokemon->nombre_pokemon,get_pokemon->id_mensaje);
+}
+
+void atender_catchPokemon(int socket){
+	t_catch_pokemon* catch_pokemon= recibirCatchPokemon(socket);
+	log_info(logGamecard, "Recibi CATCH_POKEMON %s %d %d [%d]\n",catch_pokemon->nombre_pokemon,catch_pokemon->pos_x,catch_pokemon->pos_y,catch_pokemon->id_mensaje);
 }
--- GameCard/gamecard.h
+++ GameCard/gamecard.h
@@ -11,14 +11,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include "protocolo.h"
 #include "conexion.h"
 #include "serializacion.h"
 #include <commons/bitarray.h>
 #include <commons/config.h>
+#include <commons/string.h>
+#include <commons/txt.h>
 #include <pthread.h>
 
 char* pathGamecardConfig = "gameCard.config";
+
+typedef struct{
+	char* directory;
+	int size;
+	int* blocks;
+	char* open;
+}t_metadata;
 
 typedef struct{
 	int tiempo_reintento_conexion;
@@ -30,5 +41,22 @@ typedef struct{
 
 t_configuracion* config_gamecard;
 t_config* config_ruta;
+t_log* logGamecard;
+t_log* logInterno;
+
+t_metadata* data_pokemon;
+t_config* metadata;
+
+int crear_config_gamecard();
+int crear_metadata_pokemon(char* path);
+
+void iniciar_directorio_basico();
+void crear_directorio_pokemon(char* nombre_pokemon);
+bool existe_archivo(char* path);
+
+void atender_cliente(int* socket_cliente);
+void atender_newPokemon(int socket);
+void atender_getPokemon(int socket);
+void atender_catchPokemon(int socket);
 
 #endif /* GAMECARD_H_ */
